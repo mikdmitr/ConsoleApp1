795ffcf62f8f7181d7e7d16bee145e49c5be79e8
mike <mikdmitr@gmail.com>
2019-02-13 22:28:25 +0300

    Add project files.

---
+++ ConsoleApp1/FirstTestCase.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp1
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://testqa.torrow.net";
+           
+        }
+    }
+}
